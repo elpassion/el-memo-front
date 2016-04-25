b4fe605ebdd909a06777a067fd64cae3e1b11ef8
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2016-04-25 15:38:42 +0200

    add ajax call to api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,26 @@
 var Board = React.createClass({
+  getInitialState: function(){
+    return {data: ''}
+  },
+  loadGame: function(){
+    $.ajax({
+      url: 'http://elmemo-backend.herokuapp.com/new/',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
+    this.loadGame();
+    console.log(this.state.data);
     var rows = [];
     for (var i=0; i < 4; i++) {
       for (var j=0; j < 4; j++) {
-        rows.push(<Field key={i+''+j} value = {i * 4 + j} />);
+        rows.push(<Field key={i+''+j} value={i+' '+j}></Field>);
       };
       rows.push(React.createElement('br', {key: i}))
     };
@@ -17,7 +34,20 @@ var Board = React.createClass({
 
 var Field = React.createClass({
   handleClick: function() {
-    console.log('click!');
+    console.log(this.props.value);
+  },
+  postFileld: function(){
+    $.ajax({
+      url: 'http://elmemo-backend.herokuapp.com/game/' + game_id + '?field=' + field_id,
+      dataType: 'json',
+      type: 'POST',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }.bind(this)
+    });
   },
   render: function() {
     return (

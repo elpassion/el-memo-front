3a25027907b500202b38082d064dc7a659750a1a
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2016-04-25 13:40:10 +0200

    add click action to field

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,21 +3,31 @@ var Board = React.createClass({
     var rows = [];
     for (var i=0; i < 4; i++) {
       for (var j=0; j < 4; j++) {
-        rows.push(<Field key={i+''+j}/>);
+        rows.push(<Field key={i+''+j} value = {i * 4 + j} />);
       };
       rows.push(React.createElement('br', {key: i}))
     };
-    return <div>{rows}</div>;
+    return (
+      React.createElement('div', {className: "Board"},
+        rows
+      )
+    )
   }
 });
 
 var Field = React.createClass({
+  handleClick: function() {
+    console.log('click!');
+  },
   render: function() {
     return (
-      React.createElement('span', {className: "Field"},
-        "Field"
+      React.createElement('span', {className: "Field", onClick: this.handleClick},
+        this.props.value
       )
     );
+  },
+  getInitialState: function(){
+    return {value: ''}
   }
 });
 
